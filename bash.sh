4812d1f4af9ad65f7c64c1736076ad87682c3533
Iain Lowe <iain.lowe@gmail.com>
2015-12-11 21:14:41 -0500

    update bash tools

--- bash.sh
+++ bash.sh
@@ -2,10 +2,15 @@ function dbuild() {
     local __doc__="usage: dbuild [-p] NAME\nBuilds (and optionally pushes) the container called NAME\nfrom the directory in ~/.config/dbuild/dir\n(currently: $(cat ~/.config/dbuild/dir))"
 
     local OPTIND
+    local build=1
 
-    while getopts ":aph" opt; do
+    while getopts ":aAph" opt; do
         case $opt in
             a)
+                local mkalias=1
+                ;;
+            A)
+                build=0
                 local mkalias=1
                 ;;
             p)
@@ -20,17 +25,21 @@ function dbuild() {
                 ;;
         esac
     done
-    echo $OPTIND
+
     shift $(($OPTIND-1))
     
-    local name=$1
+    local name=${1:-$(basename $(pwd))}
     local repo="ilowe/$name"
     local wd="$(cat ~/.config/dbuild/dir)/$name"
 
-    { pushd $wd; docker build -t $repo .; popd; }
+    [ "$build" = "1" ] && { pushd $wd; docker build -t $repo .; popd; }
+    [ "$push" = "1" ] && docker push $repo
+    [ "$mkalias" = "1" ] && ___dbuild-mkdockeralias $name && echo "Added alias $name"
+}
 
-    [ -n "$push" ] && docker push $repo
-    [ -n "$mkalias" ] && ___dbuild-mkdockeralias $1
+function ___dbuild-rmdocker() {
+    local id=$(docker ps -a | grep ilowe/$1 | sed 's: .*::')
+    [ "$id" != "" ] && docker rm -f $id
 }
 
 function ___dbuild-mkdockeralias() {
@@ -38,13 +47,21 @@ function ___dbuild-mkdockeralias() {
     local root=$(cat ~/.config/dbuild/dir)
 
     cat <<EOF > $fname
-function $1 {
-    if [ "\$1" == "-h" ] || [ "\$1" == "--help" ]; then
+${1}() {
+    if [ "\$1" == "-h" ]; then
         echo "$(grep '^#' $root/$1/Dockerfile | head -1 | sed 's:^# ::g')"
         return
     fi
+
+    ___dbuild-rmdocker $1
 EOF
-    grep -e '^#' -e '^\s*$' $root/$1/Dockerfile | tail -n +3 | sed 's:^#\s::g' | while read line; do if echo $line | grep '^\s*$'; then break; fi; echo $line; done >> $fname
+    grep -e '^#' -e '^\s*$' $root/$1/Dockerfile | tail -n +3 | sed 's:^#\s::g' | while read line; do
+        if echo $line | grep '^\s*$'; then 
+            break
+        fi
+        echo "$line"
+    done >> $fname
+
     echo "}" >> $fname
 
     source $fname
